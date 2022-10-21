ba059729b8ffec20c0bb131731312b476a681c51
Ilona Mereuta <edoilo.3521@gmail.com>
2022-10-21 14:03:50 +0200

    ex-66

--- ex-66/my-app.js
+++ ex-66/my-app.js
@@ -1,14 +1,31 @@
-
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 
-const getTodo = async () => {
+
+async function fetchTodo(){
+
     try{
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-        if(response.ok){
-            const jsonResponse = await response.json();       
-        }
-    } catch(error){
+        const response = await fetch(url);
+        const commit = await response.json();
+        console.log(commit);
+
+        commit.map((item)=>{
+            let listEl = document.createElement('li');
+            listEl.innerHTML = item.title;
+            let ulEl = document.querySelector('.todo-list');
+            ulEl.appendChild(listEl);
+        })
+
+    }catch(error){
         console.log(error);
     }
-};
+
+}
+
+
+fetchTodo();
+
+
+
+
+
