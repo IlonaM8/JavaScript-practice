f3e7b0482fa0e5d6fe5a00a3084a96f36fda6404
Ilona Mereuta <edoilo.3521@gmail.com>
2022-10-21 13:54:01 +0200

    ex-64

---
+++ es-64/my-app.js
@@ -0,0 +1,44 @@
+const fetchButton = document.getElementById('fetch-post');
+const saveButton = document.getElementById('save-post');
+const container = document.getElementById('container');
+let obj;
+
+fetchButton.style.backgroundColor = 'c6ffdd';
+saveButton.style.backgroundColor = 'fbd786'
+
+
+
+//fetch post 
+async function fetchPost() {
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+        obj = await response.json();
+        console.log(obj);
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+//event listener
+fetchButton.addEventListener('click', fetchPost);
+
+
+// save in local storage
+
+function savePost() {
+    try{
+        
+        for (const [key, value] of Object.entries(obj)) {
+            window.localStorage.setItem(key, value);   
+        }
+        console.log('Your post has been saved in the storage');
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+saveButton.addEventListener('click', savePost);
+
+
